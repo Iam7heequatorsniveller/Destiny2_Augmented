d5e6ebf2533cceac080bbb7cd1822810fe2436bd
Iam7heequatorsniveller <34959518+Iam7heequatorsniveller@users.noreply.github.com>
2022-06-03 01:41:06 +0800

    Update AFK Legendary Shards farming.lua

--- AFK Legendary Shards farming.lua
+++ AFK Legendary Shards farming.lua
@@ -1,6 +1,6 @@
 --0.Instructions(MUST read the following):
 --1.Hardware requirements: Logitech G Hub installed / full screen 1080p / frames 60+ / latency < 150ms
---2.In-game requirements: Script starts at Master Rahool's purchase interface / more than 240000 Glimmer / 45 empty space in armor(9 for each armor) / unlocked all 5 of Midnight Oil Suit
+--2.In-game requirements: Script starts at Master Rahool's purchase interface / more than 240000 Glimmer / 45 empty spaces in Armor slots(9 for each armor) / unlocked all 5 of Midnight Oil Suit
 --3.How to Import this script:
 --  3.1.Open Logitech G Hub
 --  3.2.Switch profiles to Destiny 2
@@ -12,7 +12,7 @@
 --4.There are 2 places need to be modified by yourself, see the notes for details (at the end of script)
 --5.How to START the script: turn on the CapsLock, then click the right mouse button
 --6.How to STOP the script: turn off the CapsLock(would teminal within 15 sec) / directly Quit the Logitech G Hub
---7.Please indicate the source when reprinting, author @A1ienShoo7er(yfans@live.cn)
+--7.Please indicate the source when reprinting
 
 
 
@@ -21,10 +21,9 @@ function OnEvent(event, arg)
         A1ienShoo7er()
     end
 end
-
+ 
 function A1ienShoo7er()
     while(IsKeyLockOn("capslock")) do
-        --getPosition()
         for i = 1, 10 do
             if (IsKeyLockOn("capslock")) then
                 goBuy()
@@ -34,10 +33,11 @@ function A1ienShoo7er()
             end
         end
         goG()
-        buyG()   
+        buyG()
+        --getPosition() 
     end
 end
-
+ 
 function getPosition()
     if IsMouseButtonPressed(1) then
         x, y = GetMousePosition()
@@ -45,13 +45,13 @@ function getPosition()
         Sleep(1000)
     end
 end
-
+ 
 function reacquire()
     PressMouseButton(1)
-    Sleep(3200)
+    Sleep(3150)
     ReleaseMouseButton(1)
 end
-
+ 
 function dismantle()
     PressKey("f")
     Sleep(1200)
@@ -60,24 +60,19 @@ end
 
 function goBuy()
     if (IsKeyLockOn("capslock")) then
-        PressAndReleaseKey(0x3b)
-        Sleep(600)
-        PressAndReleaseKey("a")
-        Sleep(600)
-        PressAndReleaseKey("a")
-        Sleep(600)
+        PressAndReleaseKey("b")
         MoveMouseTo(31179,24598)
-        Sleep(600)
+        Sleep(800)
+        PressAndReleaseMouseButton(1)
+        Sleep(800)
+        MoveMouseTo(12021,50169)
+        Sleep(500)
+        PressAndReleaseMouseButton(1)
+        Sleep(250)
+        MoveMouseTo(61949,40633)
+        Sleep(250)
         PressAndReleaseMouseButton(1)
         Sleep(300)
-        MoveMouseTo(12021,50169)
-        Sleep(800)
-        PressAndReleaseMouseButton(1)
-        Sleep(800)
-        MoveMouseTo(61949,40633)
-        Sleep(800)
-        PressAndReleaseMouseButton(1)
-        Sleep(800)
     end
 end
 
@@ -85,19 +80,19 @@ function doBuy()
     for i = 1, 9 do
         if (IsKeyLockOn("capslock")) then
             MoveMouseTo(24691,53327)
-            Sleep(200)
+            Sleep(100)
             reacquire()
             MoveMouseTo(28208,53327)
-            Sleep(200)
+            Sleep(100)
             reacquire()
             MoveMouseTo(31658,52962)
-            Sleep(200)
+            Sleep(100)
             reacquire()
             MoveMouseTo(35004,52841)
-            Sleep(200)
+            Sleep(100)
             reacquire()
             MoveMouseTo(38624,52962)
-            Sleep(200)
+            Sleep(100)
             reacquire()
         end
     end
@@ -105,12 +100,8 @@ end
 
 function goF()
     if (IsKeyLockOn("capslock")) then
-        PressAndReleaseKey(0x01)
-        Sleep(600)
-        PressAndReleaseKey("d")
-        Sleep(600)
-        PressAndReleaseKey("d")
-        Sleep(800)
+        PressAndReleaseKey("x")
+        Sleep(2000)
     end
 end
 
@@ -118,48 +109,45 @@ function doF()
     for i = 1, 9 do
         if (IsKeyLockOn("capslock")) then
             MoveMouseTo(47708,16581)
-            Sleep(100)
+            Sleep(50)
             MoveMouseTo(51294,16277)
             Sleep(300)
             dismantle()
             MoveMouseTo(47640,24113)
-            Sleep(100)
+            Sleep(50)
             MoveMouseTo(51670,23930)
             Sleep(300)
             dismantle()
             MoveMouseTo(47708,31826)
-            Sleep(100)
+            Sleep(50)
             MoveMouseTo(51465,31705)
             Sleep(300)
             dismantle()
             MoveMouseTo(47606,38568)
-            Sleep(100)
+            Sleep(50)
             MoveMouseTo(51602,39054)
             Sleep(300)
             dismantle()
             MoveMouseTo(47811,46099)
-            Sleep(100)
+            Sleep(50)
             MoveMouseTo(51567,46646)
             Sleep(300)
             dismantle()
         end
     end
-    PressAndReleaseKey(0x3b)
-    Sleep(600)
 end
 
 function goG()
     if (IsKeyLockOn("capslock")) then
-    Sleep(500)
-    --PressAndReleaseKey(0x01)
-    Sleep(2500)
+        PressAndReleaseKey(0x01)
+        Sleep(3500)
     end
 end
 
 function buyG()
-    for i = 1, 24 do
+    for i = 1, 25 do
         if (IsKeyLockOn("capslock")) then
-            MoveMouseTo(50133, 11111)       --material position from left to right: 46820 / 50133 / 53547 / 56962  (would change everyday)
+            MoveMouseTo(46820, 11111)       --material position from left to right: 46820 / 50133 / 53547 / 56962  (would change everyday)
             Sleep(700)
             PressAndReleaseMouseButton(1)
             Sleep(700)
@@ -170,8 +158,9 @@ function buyG()
             PressAndReleaseMouseButton(1)
             Sleep(100)
             MoveMouseTo(39888, 11111)       --Glimmer position from left to right: 39888 / 42893   (would change everyday)
-            Sleep(750)
+            Sleep(700)
             PressAndReleaseMouseButton(1)
+            Sleep(100)
         end
     end
 end
